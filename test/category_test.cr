67537e0efbf938f5c8dd75749dcd77614ffa754c
Chris Larsen <clarsendev@protonmail.com>
2021-04-04 22:18:41 -0400

    Update category test and remove unused files

--- test/category_test.cr
+++ test/category_test.cr
@@ -5,13 +5,13 @@ require "/../src/mediawiki/category"
 
 class CategoryTest < Minitest::Test
   def test_initializes_category
-    json = File.read("test/files/iron-compounds1.json")
+    json = File.read("test/files/iron-compounds.json")
     parsed = JSON.parse(json)
     parsed_list = parsed.as_h["query"].as_h
 
     category = Wikipedia::Category.from_json(parsed_list.to_json)
 
-    assert_equal 100, category.pages.size
-    assert_equal 0, category.subcategories.size
+    assert_equal 105, category.pages.size
+    assert_equal 10, category.subcategories.size
   end
 end
